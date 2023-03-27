b0799fa0a8da2b7f222843adbf04911cb7757226
Lars Scheibling <lars@scheibling.se>
2023-03-27 10:03:20 +0000

    Bugfix in azure-upload on ubuntu-ci

--- cloudyne/ubuntu-ci/azure-upload.py
+++ cloudyne/ubuntu-ci/azure-upload.py
@@ -15,11 +15,20 @@ class Upload:
             self.destination_path = os.environ['DESTINATION_PATH']
         except KeyError as e:
             raise Exception(f"Missing environment variable: {e}")
-    
+        
+        print("Successfully set environment variables")
         self.client = BlobClient.from_blob_url(self.sas_url.replace("?", f"/{self.destination_path}?"))
+        
+        print("Successfully created client")
         self.upload_file()
+        
+        print("Successfully uploaded file")
 
     def upload_file(self):
+        print("Started uploading file...")
         with open(self.upload_file, "rb") as data:
             self.client.upload_blob(data, overwrite=True, blob_type="BlockBlob")            
 
+if __name__ == '__main__':
+    print("Starting upload....")
+    Upload()
